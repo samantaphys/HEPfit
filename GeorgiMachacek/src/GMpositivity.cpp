6340def3ee23cb9129d4ffe19ef2add8fcd4a45b
Subrata Samanta <samantaphys@Subratas-MacBook-Pro.local>
2023-09-20 10:21:22 +0530

    test commit first one!

--- GeorgiMachacek/src/GMpositivity.cpp
+++ GeorgiMachacek/src/GMpositivity.cpp
@@ -15,10 +15,23 @@ GMpositivity1::GMpositivity1(const StandardModel& SM_i)
 
 double GMpositivity1::computeThValue()
 {
+    flag_GM = myGM.getFlagGM();
+    flag_EGM = myGM.getFlagEGM();
+    flag_GGM = myGM.getFlagGGM();
+    
+    std::cout<< "flag-check4:" << flag_GM << flag_EGM << flag_GGM <<std::endl;
+    
+    if(flag_GM) {
+
     double lambda2 = myGM.getMyGMCache()->lambda2;
     double lambda3 = myGM.getMyGMCache()->lambda3;
 //    std::cout<<"p1="<<lambda2+lambda3<<std::endl;
     return lambda2+lambda3;
+    
+    } else if(flag_EGM){
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
 }
 
 
@@ -29,11 +42,22 @@ GMpositivity2::GMpositivity2(const StandardModel& SM_i)
 
 double GMpositivity2::computeThValue()
 {
+    flag_GM = myGM.getFlagGM();
+    flag_EGM = myGM.getFlagEGM();
+    flag_GGM = myGM.getFlagGGM();
+    
+    if(flag_GM) {
+         
     double lambda2 = myGM.getMyGMCache()->lambda2;
     double lambda3 = myGM.getMyGMCache()->lambda3;
 
 //    std::cout<<"p2="<<2.0*lambda2+lambda3<<std::endl;
     return 2.0*lambda2+lambda3;
+    
+    } else if(flag_EGM){
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
 }
 
 
@@ -44,6 +68,12 @@ GMpositivity3::GMpositivity3(const StandardModel& SM_i)
 
 double GMpositivity3::computeThValue()
 {
+    flag_GM = myGM.getFlagGM();
+    flag_EGM = myGM.getFlagEGM();
+    flag_GGM = myGM.getFlagGGM();
+    
+    if(flag_GM) {
+         
     double lambda1 = myGM.getMyGMCache()->lambda1;
     double lambda2 = myGM.getMyGMCache()->lambda2;
     double lambda3 = myGM.getMyGMCache()->lambda3;
@@ -59,6 +89,12 @@ double GMpositivity3::computeThValue()
     }
 //    std::cout<<"p3="<<pos3<<std::endl;
     return pos3;
+    
+    } else if(flag_EGM){
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
+    
 }
 
 
@@ -69,6 +105,12 @@ GMpositivity4::GMpositivity4(const StandardModel& SM_i)
 
 double GMpositivity4::computeThValue()
 {
+    flag_GM = myGM.getFlagGM();
+    flag_EGM = myGM.getFlagEGM();
+    flag_GGM = myGM.getFlagGGM();
+    
+    if(flag_GM) {
+  
     double lambda1 = myGM.getMyGMCache()->lambda1;
     double lambda2 = myGM.getMyGMCache()->lambda2;
     double lambda3 = myGM.getMyGMCache()->lambda3;
@@ -85,4 +127,200 @@ double GMpositivity4::computeThValue()
     }
 //    std::cout<<"p4="<<pos4<<std::endl;
     return pos4;
+    
+     } else if(flag_EGM){
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
 }
+
+/**
+EGMpositivity1::EGMpositivity1(const StandardModel& SM_i)
+: ThObservable(SM_i), myGM(static_cast<const GeorgiMachacek&> (SM_i))
+{}
+
+double EGMpositivity1::computeThValue()
+{
+    flag_GM = myGM.getFlagGM();
+    flag_EGM = myGM.getFlagEGM();
+    flag_GGM = myGM.getFlagGGM();
+    
+    
+    if(flag_GM) {
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+ 
+     } else if(flag_EGM){
+         
+    double lambda1 = myGM.getMyGMCache()->lambda1;
+    double lambda2 = myGM.getMyGMCache()->lambda2;
+    double lambda3 = myGM.getMyGMCache()->lambda3;
+    double lambda4 = myGM.getMyGMCache()->lambda4;
+    double lambda5 = myGM.getMyGMCache()->lambda5;
+    double pos4 = -1.0;
+    if(lambda1>0 && 2.0*lambda2+lambda3>0)
+    {
+        pos4 = lambda4-0.25*fabs(lambda5)+sqrt(2.0*lambda1*(2.0*lambda2+lambda3));
+    }
+    else
+    {
+        pos4 = -fabs(lambda1*(2.0*lambda2+lambda3));
+    }
+//    std::cout<<"p4="<<pos4<<std::endl;
+    return pos4;
+        
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
+}
+
+EGMpositivity2::EGMpositivity2(const StandardModel& SM_i)
+: ThObservable(SM_i), myGM(static_cast<const GeorgiMachacek&> (SM_i))
+{}
+
+double EGMpositivity2::computeThValue()
+{
+    flagGM = myGM.getflagGM();
+    flagEGM = myGM.getflagEGM();
+    flagGGM = myGM.getflagGGM();
+    flagLO = myGM.getflagLO();
+    flagNLO = myGM.getflagNLO();
+    
+    if(flagGM) {
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+ 
+     } else if(flagEGM){
+         
+    double lambda1 = myGM.getMyGMCache()->lambda1;
+    double lambda2 = myGM.getMyGMCache()->lambda2;
+    double lambda3 = myGM.getMyGMCache()->lambda3;
+    double lambda4 = myGM.getMyGMCache()->lambda4;
+    double lambda5 = myGM.getMyGMCache()->lambda5;
+    double pos4 = -1.0;
+    if(lambda1>0 && 2.0*lambda2+lambda3>0)
+    {
+        pos4 = lambda4-0.25*fabs(lambda5)+sqrt(2.0*lambda1*(2.0*lambda2+lambda3));
+    }
+    else
+    {
+        pos4 = -fabs(lambda1*(2.0*lambda2+lambda3));
+    }
+//    std::cout<<"p4="<<pos4<<std::endl;
+    return pos4;
+        
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
+}
+
+EGMpositivity3::EGMpositivity3(const StandardModel& SM_i)
+: ThObservable(SM_i), myGM(static_cast<const GeorgiMachacek&> (SM_i))
+{}
+
+double EGMpositivity3::computeThValue()
+{
+    flagGM = myGM.getflagGM();
+    flagEGM = myGM.getflagEGM();
+    flagGGM = myGM.getflagGGM();
+    flagLO = myGM.getflagLO();
+    flagNLO = myGM.getflagNLO();
+    
+    if(flagGM) {
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+ 
+     } else if(flagEGM){
+         
+    double lambda1 = myGM.getMyGMCache()->lambda1;
+    double lambda2 = myGM.getMyGMCache()->lambda2;
+    double lambda3 = myGM.getMyGMCache()->lambda3;
+    double lambda4 = myGM.getMyGMCache()->lambda4;
+    double lambda5 = myGM.getMyGMCache()->lambda5;
+    double pos4 = -1.0;
+    if(lambda1>0 && 2.0*lambda2+lambda3>0)
+    {
+        pos4 = lambda4-0.25*fabs(lambda5)+sqrt(2.0*lambda1*(2.0*lambda2+lambda3));
+    }
+    else
+    {
+        pos4 = -fabs(lambda1*(2.0*lambda2+lambda3));
+    }
+//    std::cout<<"p4="<<pos4<<std::endl;
+    return pos4;
+        
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
+}
+
+EGMpositivity4::EGMpositivity4(const StandardModel& SM_i)
+: ThObservable(SM_i), myGM(static_cast<const GeorgiMachacek&> (SM_i))
+{}
+
+double EGMpositivity4::computeThValue()
+{
+    flagGM = myGM.getflagGM();
+    flagEGM = myGM.getflagEGM();
+    flagGGM = myGM.getflagGGM();
+    flagLO = myGM.getflagLO();
+    flagNLO = myGM.getflagNLO();
+    
+    if(flagGM) {
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+ 
+     } else if(flagEGM){
+         
+    double lambda1 = myGM.getMyGMCache()->lambda1;
+    double lambda2 = myGM.getMyGMCache()->lambda2;
+    double lambda3 = myGM.getMyGMCache()->lambda3;
+    double lambda4 = myGM.getMyGMCache()->lambda4;
+    double lambda5 = myGM.getMyGMCache()->lambda5;
+    double pos4 = -1.0;
+    if(lambda1>0 && 2.0*lambda2+lambda3>0)
+    {
+        pos4 = lambda4-0.25*fabs(lambda5)+sqrt(2.0*lambda1*(2.0*lambda2+lambda3));
+    }
+    else
+    {
+        pos4 = -fabs(lambda1*(2.0*lambda2+lambda3));
+    }
+//    std::cout<<"p4="<<pos4<<std::endl;
+    return pos4;
+        
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
+}
+
+EGMpositivity5::EGMpositivity5(const StandardModel& SM_i)
+: ThObservable(SM_i), myGM(static_cast<const GeorgiMachacek&> (SM_i))
+{}
+
+double EGMpositivity5::computeThValue()
+{
+    flagGM = myGM.getflagGM();
+    flagEGM = myGM.getflagEGM();
+    flagGGM = myGM.getflagGGM();
+    flagLO = myGM.getflagLO();
+    flagNLO = myGM.getflagNLO();
+    
+    if(flagGM) {
+        throw std::runtime_error("WARNING: wrong observables for EGM model");
+ 
+     } else if(flagEGM){
+         
+    double lambda1 = myGM.getMyGMCache()->lambda1;
+    double lambda2 = myGM.getMyGMCache()->lambda2;
+    double lambda3 = myGM.getMyGMCache()->lambda3;
+    double lambda4 = myGM.getMyGMCache()->lambda4;
+    double lambda5 = myGM.getMyGMCache()->lambda5;
+    double pos4 = -1.0;
+    if(lambda1>0 && 2.0*lambda2+lambda3>0)
+    {
+        pos4 = lambda4-0.25*fabs(lambda5)+sqrt(2.0*lambda1*(2.0*lambda2+lambda3));
+    }
+    else
+    {
+        pos4 = -fabs(lambda1*(2.0*lambda2+lambda3));
+    }
+//    std::cout<<"p4="<<pos4<<std::endl;
+    return pos4;
+        
+    } else
+        throw std::runtime_error("WARNING: wrong observables for GGM model");
+}
+ */
